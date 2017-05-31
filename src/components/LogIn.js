d4899313218a11bf872fd3abc80ec1d53734db17
Marko <marko2606@hotmail.com>
2017-05-31 12:02:10 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/components/LogIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class LogIn extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Log in </Text>
+            </View>
+        )
+    }
+}
+
+export default LogIn;
