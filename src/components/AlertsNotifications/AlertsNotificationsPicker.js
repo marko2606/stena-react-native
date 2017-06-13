380ae8ee061b93c65d11a635b8f99ef8cde4399e
nemanja.manot <n.manot@yahoo.com>
2017-06-13 15:56:00 +0200

    start with dropdown filter

--- src/components/AlertsNotifications/AlertsNotificationsPicker.js
+++ src/components/AlertsNotifications/AlertsNotificationsPicker.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Picker, View } from 'react-native';
+import { View } from 'react-native';
+
+import { colors } from '../../Colors';
+import { Dropdown } from '../../shared/';
+
+const filterGroup = [
+    'ALL VESSELS',
+    'MY FLEET',
+    'SUEZMAX',
+    'STENA BULK',
+    'STENA WECO',
+    'GOLDEN STENA WECO',
+    'STENA LNG',
+    'MORE OPTIONS',
+    'MORE OPTIONS',
+];
 
 const AlertsNotificationsPicker = (props) => {
     return (
         <View style={props.style.pickerContainer}>
-            <Picker
-                selectedValue={props.selectedValue}
-                onValueChange={props.onValueChange.bind(this)}
-                style={props.style.pickerStyle}
-            >
-                <Picker.Item label="Filter Alerts by group..." value="initial" />
-                <Picker.Item label="ALL VESSELS" value="allVessels" />
-                <Picker.Item label="MY FLEET" value="myFleet" />
-                <Picker.Item label="SUEZMAX" value="suezmax" />
-                <Picker.Item label="STENA BULK" value="stenaBulk" />
-                <Picker.Item label="STENA WECO" value="stenaWeco" />
-                <Picker.Item label="GOLDEN STENA WECO" value="goldenStenaWeco" />
-                <Picker.Item label="STENA LNG" value="stenaLng" />
-            </Picker>
+            <Dropdown
+                dropdownStyle={{ flex: 1, width: '91%', height: '60%', backgroundColor: 'transparent', borderWidth: 0 }}
+                options={filterGroup}
+                onSelect={ (index) => console.log(index)} // TODO
+                dropdownTextStyle={{ fontSize: 16, padding: 5, color: colors['whiteColor'], backgroundColor: colors['primaryColor2'] }}
+                textStyle={{ color: colors['grayColor'], padding: 12, fontSize: 13, borderColor: 'red' }}
+                defaultValue="Filter Alerts by group"
+                dropdownTextHighlightStyle={{ color: colors['primaryColor1'] }}
+            />
         </View>
     )
 };
---
+++ src/shared/ModalDropdown/ModalDropdown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ModalDropdown from 'react-native-modal-dropdown';
+
+
+export const Dropdown = (props) => {
+    return (
+        <ModalDropdown
+            dropdownStyle={props.dropdownStyle}
+            options={props.options}
+            onSelect={props.onSelect}
+            dropdownTextStyle={props.dropdownTextStyle}
+            textStyle={props.textStyle}
+            defaultValue={props.defaultValue}
+            dropdownTextHighlightStyle={props.dropdownTextHighlightStyle}
+        />
+    )
+};
