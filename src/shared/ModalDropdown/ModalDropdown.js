f6a9781a8e76167c4dde5e997062ba4ee91feffa
nemanja.manot <n.manot@yahoo.com>
2017-06-14 12:21:59 +0200

    destructure ModalDropdown

--- src/shared/ModalDropdown/ModalDropdown.js
+++ src/shared/ModalDropdown/ModalDropdown.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ModalDropdown from 'react-native-modal-dropdown';
 
 export const Dropdown = (props) => {
+    const {
+        dropdownStyle, options,
+        onSelect, dropdownTextStyle,
+        textStyle, defaultValue,
+        dropdownTextHighlightStyle, renderSeparator
+    } = props;
+
     return (
         <ModalDropdown
-            dropdownStyle={props.dropdownStyle}
-            options={props.options}
-            onSelect={props.onSelect}
-            dropdownTextStyle={props.dropdownTextStyle}
-            textStyle={props.textStyle}
-            defaultValue={props.defaultValue}
-            dropdownTextHighlightStyle={props.dropdownTextHighlightStyle}
-            renderSeparator={props.renderSeparator}
+            dropdownStyle={dropdownStyle}
+            options={options}
+            onSelect={onSelect}
+            dropdownTextStyle={dropdownTextStyle}
+            textStyle={textStyle}
+            defaultValue={defaultValue}
+            dropdownTextHighlightStyle={dropdownTextHighlightStyle}
+            renderSeparator={renderSeparator}
         />
     )
 };
