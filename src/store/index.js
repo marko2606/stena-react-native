7fb68f42765e4135aad65049891678ca7172f09e
Marko <marko2606@hotmail.com>
2017-06-15 16:47:18 +0200

    reduxDevTool added

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,20 @@ import {
     createStore,
     applyMiddleware
 } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
 import reducers from '../reducers';
 
 
+const composeEnhancers = composeWithDevTools({realtime: true});
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     reducers,
-    applyMiddleware(sagaMiddleware)
+    composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
 sagaMiddleware.run(rootSaga);
 
 export default store;
